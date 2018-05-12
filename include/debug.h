c43b4f5a448c045f7ca733212d4619e2eb5f0d73
tracer <tracer@tracer.org>
2018-05-11 23:57:04 -0700

    > build wallaceyin Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- include/debug.h
+++ include/debug.h
@@ -6,6 +6,8 @@ static char log_out[1024];
 void Log(const char *fmt, ...) {
 	printf("\033[34m");
 	memset(log_out, 0, 1024);
+	va_list ap;
+	va_start(ap, fmt);
 	vsprintf(log_out, fmt, ap);
 	for (int i = 0; i < strlen(log_out); i++)
 		_putc(log_out[i]);
