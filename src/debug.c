675418a537a4067da4ceaf65d5401f142837ee1f
tracer <tracer@tracer.org>
2018-05-13 19:32:23 -0700

    > build wallaceyin Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

---
+++ src/debug.c
@@ -0,0 +1,21 @@
+#include <debug.h>
+void Log(const char *fmt, ...) {
+	printf("\033[34m");
+	memset(log_out, 0, 1024);
+	va_list ap;
+	va_start(ap, fmt);
+	vsprintf(log_out, fmt, ap);
+	for (int i = 0; i < strlen(log_out); i++)
+		_putc(log_out[i]);
+	va_end(ap);
+	printf("\n");
+	printf("\033[0m");
+}
+
+void perror(const char *fmt) {
+	printf("\033[31m");
+	for (char *p = (char *)fmt; *p; p++)
+		_putc(*p);
+	printf("\n");
+	printf("\033[0m");
+}
