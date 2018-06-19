9e8627900d75bff008001fb90d791793730a5a74
tracer <tracer@tracer.org>
2018-06-18 21:41:25 -0700

    > run wallaceyin Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -107,6 +107,7 @@ static void vfs_create(filesystem_t *fs, char *path, int flags) {
 		fs->Filemap[fs->num_file].inode.block[i] = NULL;
 	if (strcmp("/proc/cpuinfo", path) == 0)
 	{
+		Log("Why?");
 		fs->Filemap[fs->num_file].inode.num_block = 1;
 		fs->Filemap[fs->num_file].inode.block[0] = (void *)pmm->alloc(PIECE_SIZE);
 		memcpy(fs->Filemap[fs->num_file].inode.block[0], cpuinfo, strlen(cpuinfo));
