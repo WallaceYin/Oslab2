ad82df04730e2905036ceb2e2c38b492c82b2056
tracer <tracer@tracer.org>
2018-06-18 17:36:59 -0700

    > build wallaceyin Linux ubuntu 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -67,6 +67,7 @@ static void printinfo(void *arg) {
 	int fd = vfs->open("/proc/cpuinfo", O_RDONLY);
 	memset(s, 0, 30);
 	vfs->read(fd, (void *)s, 30);
+	_putc('a');
 	for (int i = 0; i < strlen(s); i++)
 		_putc(s[i]);
 }
